1450b5409d1288c8e9c602db8f15036ee3ff334a
EuilimChoi <91925895+EuilimChoi@users.noreply.github.com>
2023-07-22 18:39:08 +0900

    develop

--- src/app.service.ts
+++ src/app.service.ts
@@ -15,60 +15,51 @@ export class AppService {
         waitUntil: 'networkidle0',
       });
       // 메뉴 정보 크롤링
-      const menuElements = await page.$$eval(
-        '#mArticle > div.cont_menu > ul > li.nophoto_type',
-        (options) => {
-          return options.map((option) => option.innerText);
-        },
-      );
-      const aa = await page.$$(
+      const webMenuList = await page.$$(
         '#mArticle > div.cont_menu > ul > li.nophoto_type',
       );
 
-      const bb = await aa[0].$eval(
-        'div.info_menu > em.price_menu',
-        (data) => data.textContent,
-      );
+      const menuList: [string, number][] = [];
+      for (const menu of webMenuList) {
+        //메뉴 이름
+        const menuName = await menu.$eval(
+          'div.info_menu > span.loss_word',
+          (data) => data.textContent,
+        );
 
-      const cc = Number(bb.replace('가격: ', '').replace(',', ''));
+        //메뉴 가격
+        const menuPirce = await menu
+          .$eval('div.info_menu > em.price_menu', (data) => data.textContent)
+          .then((x) => Number(x.replace('가격: ', '').replace(',', '')));
 
-      console.log(cc);
+        menuList.push([menuName, menuPirce]);
+      }
 
-      // console.log(menuElements);
-      console
-        .log
-        // await aa[0].$eval(
-        //   'div.info_menu > span.loss_word',
-        //   (data) => data.textContent,
-        // ),
-
-        // await aa[0]
-        //   .$eval('div.info_menu > em.price_menu', (data) => data.textContent)
-        //   .then((a) => {
-        //     return a.replace('가격 : ', '');
-        //   }),
-        ();
-      const menus = [];
-      // for (const menuElement of menuElements) {
-      //   const menuName = await menuElement.$eval(
-      //     '.loss_word',
-      //     (element) => element.textContent,
-      //   );
-      //   const menuPrice = await menuElement.$eval(
-      //     '.price',
-      //     (element) => element.textContent,
-      //   );
-      //   menus.push({ name: menuName, price: menuPrice });
-      // }
-      console.log('메뉴 정보:', menus);
+      console.log(menuList);
 
       // 음식 사진 크롤링
-      const photoElements = await page.$$('.thumb_area .thumb_img');
+      // await page.click('li.size_l');
+      const src = await page.$$(
+        '#mArticle > div.cont_photo > div.photo_area > ul > li',
+      );
+
+      for (const img of src) {
+        const mm = await img.$('a');
+        const el = await mm.evaluate((el) => {
+          return el.getAttribute('style');
+        });
+
+        const urlMatch = el.match(/url\('(.+)'\)/);
+        const imageUrl = urlMatch ? urlMatch[1] : null;
+        console.log(imageUrl);
+      }
+      // const photoElements = await page.$$('.thumb_area .thumb_img');
       const photos = [];
       // for (const photoElement of photoElements) {
       //   const photoUrl = await photoElement.evaluate((element) => element.src);
       //   photos.push(photoUrl);
       // }
+
       console.log('음식 사진:', photos);
     } catch (error) {
       console.log('오류 발생:', error);
